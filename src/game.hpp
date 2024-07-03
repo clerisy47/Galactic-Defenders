c5b27e34a8e789dc71bb7250be07042168f84ec8
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-07-03 18:03:27 +0545

    Add alien movement and shooting

--- src/game.hpp
+++ src/game.hpp
@@ -1,21 +1,31 @@
 #pragma once
-#include"spaceship.hpp"
+#include "spaceship.hpp"
 #include "obstacles.hpp"
+#include "aliens.hpp"
 
-class Game {
-	private:
-		Spaceship player;
-		EnemySpaceship enemy;
-		std::vector<Obstacle> obstacles;
-		std::vector<Obstacle> createObstacles();
-		void deleteInactiveLasers();
-		int level = 0;
+class Game
+{
+private:
+	Spaceship player;
+	EnemySpaceship enemy;
+	std::vector<Obstacle> obstacles;
+	std::vector<Obstacle> createObstacles();
+	std::vector<Alien> createAliens();
+	void deleteInactiveLasers();
+	void MoveAliens();
+	void MoveDownAliens(int distance);
+	void AlienShoot();
+	int level = 0;
+	int alienDirection;
+	std::vector<Alien> alienVector;
+	std::vector<Laser> alienLasers;
+	constexpr static float alienShootInterval = 0.35;
+	float timeLastAlienFired;
+
 public:
 	Game();
 	~Game();
 	void draw();
 	void update();
 	void handleInput();
-
-
 };
