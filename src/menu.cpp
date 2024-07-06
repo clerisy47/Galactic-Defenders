d4cfb0b97a8e8af5b2709d8e4c3b917157f0aaca
ShrineSigdel <123295702+ShrineSigdel@users.noreply.github.com>
2024-07-06 13:24:40 +0545

    Modified menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -3,8 +3,8 @@
 
 Menu::Menu(Texture2D bg)
     : background(bg),
-      startButton("../assets/buttons/start.png", {static_cast<float>(Window::width / 4 - 100), static_cast<float>(Window::height / 2 - 100)}),
-      exitButton("../assets/buttons/quit.png", {static_cast<float>(2 * Window::width / 4 - 100), static_cast<float>(Window::height / 2 - 100)})
+      startButton("../assets/buttons/start.png", {static_cast<float>(Window::width / 2 - 205), static_cast<float>(Window::height / 2 - 100)}),
+      exitButton("../assets/buttons/quit.png", {static_cast<float>(Window::width / 2 - 205), static_cast<float>(Window::height / 2 + 100)})
 {
 }
 
@@ -29,6 +29,7 @@ void Menu::Draw()
 {
     ClearBackground(RAYWHITE);
     DrawTexture(background, 0, 0, WHITE);
+    DrawTextEx(font, "GALACTIC INVADERS", {static_cast<float>(Window::width / 2 - 400), 150}, 100, 2, WHITE);
     startButton.Draw();
     exitButton.Draw();
 }
